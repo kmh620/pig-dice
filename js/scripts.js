8b58c36e93918503ea7a612d05d65b6df667a31b
Katrina Hockman and leo mangutov <katrina@>
2018-10-31 16:20:55 -0700

    Attempt add player turn functionality, have number generator, click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,69 @@
-// function DiceRoll(){
-//   this.rolls = []
-// }
 
 
+function Player(namePlayer) {
+  this.namePlayer = namePlayer,
+  this.turnTotal = 0,
 
-function PlayerOne (namePlayerOne, turnTotal, scoreTotal) {
-  this.namePlayerOne = namePlayerOne,
-  this.turnTotal = turnTotal,
-  this.scoreTotal = 0,
-  this.rolls = []
-  console.log(this.rolls)
+  this.gameTotal = 0
 }
 
-PlayerOne.prototype.addRoll = function (randomNumber) {
-  this.rolls.push(randomNumber);
+
+
+Player.prototype.playerTurn = function(roll) {
+    if (roll === 1) {
+      this.turnTotal = 0
+      console.log("it got here");
+    } else {
+      this.turnTotal += roll;
+      console.log("it got here");
+      }
+      console.log("you reached the end");
 }
 
-PlayerOne.prototype.findRoll = function() {
-  for (var i = 0; (this.rolls).length; i++){
-    if ([i] >2 && [i] <6) {
-      this.turnTotal += [i];
-      //add number to turntotal
-    } else if ([i] === 1) {
-      //end turn, delete turntotal, no add #
-    }
+Player.prototype.playerEnd = function() {
+  this.gameTotal += this.turnTotal;
+  console.log(this.gameTotal);
+ //player holds, end turn add turn total to game total,
+  //if game total <100+ player wins game over!! ??
   }
-}
-
-
-
 
 function randomNumber() {
-    var newRoll = Math.floor((Math.random() * 6) + 1);
+  return Math.floor((Math.random() * 6) + 1);
   }
 
+
+var Player1 = new Player("john");
+var Player2 = new Player("allen");
 
 //var  = new DiceRoll();
 
   //UI
   $(document).ready(function(){
-    $("#pig-dice").submit(function(event){
+    $("#play1").click(function() {
+      var roll = Player1.playerTurn();
+    };
+
+      $("#result1").text(roll);
+    })
+    //var total = Player1.playerEnd ??
+    $("#hold1").submit(function(event){
       event.preventDefault();
 
-      //var newRoll = Math.floor((Math.random() * 6) + 1);
-      // $("#result").text();
+      $("#turn-total1").text(Player.gameTotal);
     })
+
+
+    $("#play2").submit(function(event){
+      event.preventDefault();
+      $("#result2").text(roll);
+    })
+
+    $("#hold2").submit(function(event){
+      event.preventDefault();
+      Player1.playerEnd();
+      $("#turn-total2").text(Player.gameTotal);
+    })
+
+
+
   })
